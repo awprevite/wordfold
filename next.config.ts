38589d702e8dd2652561515b5de5480e1fe96ae4
awprevite <awprevite@gmail.com>
2025-03-22 12:37:03 -0400

    Ready for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
+// To use npm run dev, the contents of this function may need to be deleted
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // Converts Next.js to static files
   distDir: "out",   // Default directory for GitHub Pages
-  basePath: "/wordfold"
+  basePath: "/wordfold",
+  assetPrefix: "/wordfold",  // Ensures assets load correctly
 };
 
 export default nextConfig;
