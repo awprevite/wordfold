b5c16f14a6cd37cab9b39f9ec47ba544ad733193
awprevite <awprevite@gmail.com>
2025-03-22 12:48:45 -0400

    Deploying with vercel

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-// To use npm run dev, the contents of this function may need to be deleted
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export", // Converts Next.js to static files
-  distDir: "out",   // Default directory for GitHub Pages
-  basePath: "/wordfold",
-  assetPrefix: "/wordfold",  // Ensures assets load correctly
+
 };
 
 export default nextConfig;
